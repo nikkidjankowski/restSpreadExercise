a2f9a5cf70321d2ddf5a86b530527d2ccc512a5d
nikkidjankowski <79601782+nikkidjankowski@users.noreply.github.com>
2022-02-17 14:46:47 -0600

    Add files via upload

---
+++ restSpread.js
@@ -0,0 +1,81 @@
+/*
+function filterOutOdds() {
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function(num) {
+      return num % 2 === 0
+    });
+  }
+
+function removeRandom(items) {
+    let idx = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, idx), ...items.slice(idx + 1)];
+}
+
+
+
+function extend(array1, array2) {
+    return [...array1, ...array2]
+}
+*/
+
+  const filterOutOdds = (...args) => args.filter(v => v % 2 ===0);
+
+
+const findMin = (...args) => Math.min(...args);
+
+const mergeObjects = (obj, obj2) => ({...obj, ...obj2});
+
+const doubleAndReturnArgs = (arr, ...more) => [...arr, ...more.map(v => v *2)];
+
+
+
+/** remove a random element in the items array
+and return a new array without that item. */
+
+
+const removeRandom = items => {
+    let idx = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, idx), ...items.slice(idx + 1)];
+}
+
+
+/** Return a new array with every item in array1 and array2. */
+
+
+const extend = (array1, array2) => [...array1, ...array2];
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+
+const addKeyVal = (obj, key, val) => {
+    let newObj = { ...obj }
+    newObj[key] = val;
+    return newObj;
+}
+
+/** Return a new object with a key removed. */
+
+
+const removeKey = (obj, key) => {
+    let newObj = { ...obj }
+    delete newObj[key]
+    return newObj;
+
+  }
+
+/** Combine two objects and return a new object. */
+const combine = (obj1, obj2) => {
+    return { ...obj1, ...obj2 };
+  }
+
+
+
+/** Return a new object with a modified key and value. */
+
+
+const update = (obj, key, val) => {
+    let newObj = { ...obj }
+    newObj[key] = val;
+    return newObj;
+  }
